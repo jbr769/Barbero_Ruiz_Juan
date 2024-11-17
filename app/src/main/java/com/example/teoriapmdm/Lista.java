4f34eed2db25a1231d841bfc46483858da0c9a09
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-11-17 13:00:43 +0100

    Fin UD 2 Spinner y Toast

---
+++ app/src/main/java/com/example/teoriapmdm/Lista.java
@@ -0,0 +1,15 @@
+package com.example.teoriapmdm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Lista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+
+}
--- app/src/main/java/com/example/teoriapmdm/MainActivity.java
+++ app/src/main/java/com/example/teoriapmdm/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.teoriapmdm;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_Registro;
+    Button btn_Lista;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn_Registro = (Button) findViewById(R.id.btnRegistro);
+        btn_Lista = (Button) findViewById(R.id.btnLista);
+
+        btn_Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registro = new Intent(getApplicationContext(), Registro.class);
+                startActivity(registro);
+            }
+        });
+
+        btn_Lista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Lista.class);
+                startActivityForResult(intent,1);
+            }
+        });
 
     }
 }
