a74375484f3cc5b0deb049ba5a093a78e712e24e
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-11-18 13:21:11 +0100

    Fin UD 2 Spinner y Toast

--- app/src/main/java/com/example/teoriapmdm/MainActivity.java
+++ app/src/main/java/com/example/teoriapmdm/MainActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn_Registro;
     Button btn_Lista;
-
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -39,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Lista.class);
-                startActivityForResult(intent,1);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/example/teoriapmdm/Registro.java
+++ app/src/main/java/com/example/teoriapmdm/Registro.java
@@ -1,5 +1,6 @@
 package com.example.teoriapmdm;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,66 +12,78 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Registro extends AppCompatActivity {
 
-    EditText nombre;
-    EditText email;
-    EditText edad;
-    Button btnregi;
+        EditText nombre;
+        EditText email;
+        EditText edad;
+        Button btnregi;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+        @Override
+        protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_registro);
 
-        nombre = (EditText) findViewById(R.id.txtNombre);
-        email = (EditText) findViewById(R.id.txtEmail);
-        edad = (EditText) findViewById(R.id.txtEdad);
-        btnregi = (Button) findViewById(R.id.btnRegistrarse);
+            nombre = (EditText) findViewById(R.id.txtNombre);
+            email = (EditText) findViewById(R.id.txtEmail);
+            edad = (EditText) findViewById(R.id.txtEdad);
+            btnregi = (Button) findViewById(R.id.btnRegistrase);
 
-        nombre.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if(b){
-                    if (nombre.getText().toString().equalsIgnoreCase("NOMBRE")) nombre.setText("");
-                }else{
-                    if (nombre.getText().toString().isEmpty()) nombre.setText("NOMBRE");
+            nombre.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean b) {
+                    if(b){
+                        if (nombre.getText().toString().equalsIgnoreCase("NOMBRE")) nombre.setText("");
+                    }else{
+                        if (nombre.getText().toString().isEmpty()) nombre.setText("NOMBRE");
+                    }
                 }
-            }
-        });
+            });
 
-        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if (b){
-                    if (email.getText().toString().equalsIgnoreCase("EMAIL")) email.setText("");
-                }else{
-                    if (email.getText().toString().isEmpty()) email.setText("EMAIL");
+            email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean b) {
+                    if (b){
+                        if (email.getText().toString().equalsIgnoreCase("EMAIL")) email.setText("");
+                    }else{
+                        if (email.getText().toString().isEmpty()) email.setText("EMAIL");
+                    }
                 }
-            }
-        });
+            });
 
-        edad.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                if (b){
-                    if (edad.getText().toString().equalsIgnoreCase("EDAD")) edad.setText("");
-                }else{
-                    if (edad.getText().toString().isEmpty()) edad.setText("EDAD");
+            edad.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean b) {
+                    if (b){
+                        if (edad.getText().toString().equalsIgnoreCase("EDAD")) edad.setText("");
+                    }else{
+                        if (edad.getText().toString().isEmpty()) edad.setText("EDAD");
+                    }
                 }
-            }
-        });
+            });
 
-        btnregi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (nombre.getText().toString().equalsIgnoreCase("NOMBRE") || email.getText().toString().equalsIgnoreCase("EMAIL") || edad.getText().toString().equalsIgnoreCase("EDAD")){
-                    Toast.makeText(Registro.this, "Debe rellenar todos los campos", Toast.LENGTH_LONG).show();
-                }else if (Integer.parseInt(edad.getText().toString()) < 18){
-                    Toast.makeText(Registro.this, "Debe ser mayor de edad para registrarse", Toast.LENGTH_LONG).show();
-                }else{
-
+            btnregi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (nombre.getText().toString().equalsIgnoreCase("NOMBRE") || email.getText().toString().equalsIgnoreCase("EMAIL") || edad.getText().toString().equalsIgnoreCase("EDAD")){
+                        Toast.makeText(com.example.teoriapmdm.Registro.this, "Debe rellenar todos los campos", Toast.LENGTH_LONG).show();
+                    }else if (Integer.parseInt(edad.getText().toString()) < 18){
+                        Toast.makeText(com.example.teoriapmdm.Registro.this, "Debe ser mayor de edad para registrarse", Toast.LENGTH_LONG).show();
+                    }else{
+                        AlertDialog registro = crearAlert();
+                        registro.show();
+                    }
                 }
+            });
+        }
 
-            }
-        });
-    }
+        private AlertDialog crearAlert() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(com.example.teoriapmdm.Registro.this);
+            builder.setMessage("Registro correcto");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            return builder.create();
+        }
 }
