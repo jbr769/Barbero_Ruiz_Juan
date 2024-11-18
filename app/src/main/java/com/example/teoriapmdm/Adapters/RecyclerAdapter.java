415228a45fa7713a7030fbf68561756aa96832e7
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-11-18 22:06:17 +0100

    Fin UD 2 Spinner y Toast

--- app/src/main/java/com/example/teoriapmdm/Adapters/RecyclerAdapter.java
+++ app/src/main/java/com/example/teoriapmdm/Adapters/RecyclerAdapter.java
@@ -3,6 +3,8 @@ package com.example.teoriapmdm.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +14,7 @@ import com.example.teoriapmdm.R;
 
 import java.util.List;
 
-/*public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder> {
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder> {
     List<Jugadores> listJugadores;
 
     public RecyclerAdapter(List<Jugadores> listJugadores){ this.listJugadores = listJugadores; }
@@ -34,4 +36,20 @@ import java.util.List;
     public int getItemCount() {
         return 0;
     }
-}*/
+
+    public class RecyclerHolder extends RecyclerView.ViewHolder {
+        ImageView imgJugador;
+        TextView txtViewTitle;
+        TextView  txtViewDesc;
+
+        public RecyclerHolder(@NonNull View itemView) {
+            super(itemView);
+
+            /*imgJugador  = (ImageView) itemView.findViewById(R.id.img_item);
+            txtViewTitle = (TextView)  itemView.findViewById(R.id.txt_item_tittle);
+            txtViewDesc  = (TextView)  itemView.findViewById(R.id.txt_item_desc);*/
+
+
+        }
+    }
+}
