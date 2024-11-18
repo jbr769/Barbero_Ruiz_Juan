14a5f696659c080e899e339fcee68a7ee21edaa5
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-11-18 22:02:13 +0100

    Fin UD 2 Spinner y Toast

---
+++ app/src/main/java/com/example/teoriapmdm/Adapters/RecyclerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.teoriapmdm.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.teoriapmdm.Modelos.Jugadores;
+import com.example.teoriapmdm.R;
+
+import java.util.List;
+
+/*public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder> {
+    List<Jugadores> listJugadores;
+
+    public RecyclerAdapter(List<Jugadores> listJugadores){ this.listJugadores = listJugadores; }
+
+    @NonNull
+    @Override
+    public RecyclerAdapter.RecyclerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler,parent,)
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.RecyclerHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}*/
