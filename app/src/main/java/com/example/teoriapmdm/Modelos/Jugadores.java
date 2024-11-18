f5a84180f7e571f53824be784b4124b6033776cb
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-11-18 23:52:36 +0100

    Fin UD 2 Spinner y Toast

--- app/src/main/java/com/example/teoriapmdm/Modelos/Jugadores.java
+++ app/src/main/java/com/example/teoriapmdm/Modelos/Jugadores.java
@@ -3,10 +3,10 @@ public class Jugadores {
     private String nombre;
     private String descripcion;
     private int imageId;
-    public Jugadores(int imageId, String descripcion, String nombre) {
-        this.imageId = imageId;
-        this.descripcion = descripcion;
+    public Jugadores(String nombre, String descripcion, int imagenId) {
         this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imageId = imagenId;
     }
     public String getNombre() {return nombre;}
     public void setNombre(String nombre) {this.nombre = nombre;}
