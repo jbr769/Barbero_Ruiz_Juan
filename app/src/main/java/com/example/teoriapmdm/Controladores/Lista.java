09f3d46104cfc63861e51170ca03be6510c627e2
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-11-18 22:17:43 +0100

    Fin UD 2 Spinner y Toast

--- app/src/main/java/com/example/teoriapmdm/Controladores/Lista.java
+++ app/src/main/java/com/example/teoriapmdm/Controladores/Lista.java
@@ -3,17 +3,22 @@ package com.example.teoriapmdm.Controladores;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.teoriapmdm.Adapters.RecyclerAdapter;
 import com.example.teoriapmdm.R;
 
-public class Lista extends AppCompatActivity {
+/*public class Lista extends AppCompatActivity {
+
+    RecyclerAdapter recAdapter;
+    RecyclerView recView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
-
-
+        recView = (RecyclerView) findViewById(R.id.rec)
     }
 
 }
+*/
