34c0ca0ec0185d1fec2dad870f04fa18e02ee3db
Juan Barbero Ruiz <jbarberoruiz@gmail.com>
2024-11-20 17:02:02 +0100

    Fin UD 2 Spinner y Toast

--- app/src/main/java/com/example/teoriapmdm/Controladores/Lista.java
+++ app/src/main/java/com/example/teoriapmdm/Controladores/Lista.java
@@ -3,11 +3,15 @@ package com.example.teoriapmdm.Controladores;
 import static com.example.teoriapmdm.Utilidades.ListaJugadores.getListJugadores;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.ActionMode;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -20,9 +24,9 @@ import java.util.List;
 
 public class Lista extends AppCompatActivity {
 
-    RecyclerAdapter recAdapter;
-    RecyclerView recView;
-    Toolbar toolbar;
+    private RecyclerAdapter recAdapter;
+    private RecyclerView recView;
+    private ActionMode mActionMode;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -34,7 +38,31 @@ public class Lista extends AppCompatActivity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recView.setAdapter(recAdapter);
         recView.setLayoutManager(layoutManager);
-
     }
+    private ActionMode.Callback mActionCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.menu, menu);
+            mode.setTitle("Action Menu");
+            return true;
+        }
 
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            int itemId = item.getItemId();
+
+            mode.finish();
+            return true;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+        }
+    };
 }
